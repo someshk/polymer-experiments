f5d238e42c385ff50ea002aa1d755f20d55f3987
Somesh Kumar <somesh.k@mobispectra.com>
2015-03-21 20:00:29 -0700

    Use of G+ API to get user profile on signin success

--- hello-polymer/app/scripts/app.js
+++ hello-polymer/app/scripts/app.js
@@ -10,6 +10,13 @@
   // Set the default page to home
   app.page = 'home';
 
+  // Google API Keys - TODO - May have to find the right place to set Developer API keys
+  app.googleClientId = '384334855036-gou8qpfutdijngd754lngg1cd6r59icg.apps.googleusercontent.com';
+
+  app.googleAPIKey = 'AIzaSyDlHPCEVFarkbk87GYb7X74H4zWZM49z8I';
+
+  app.googleAPIScopes = 'https://www.googleapis.com/auth/plus.me';
+
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('template-bound', function() {
